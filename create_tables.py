268a63b5f3d48f116f9a936a4d62838f0b993765
learner4657 <zohar.malina03@gmail.com>
2023-07-03 16:12:12 +0300

    add create_tables file

---
+++ create_tables.py
@@ -0,0 +1,48 @@
+import oracledb
+
+connection = oracledb.connect(
+    user="ADMIN",
+    password='wgY+J3W9vHiH',
+    wallet_location=r'C:\Users\זהר\Desktop\DBServerGit\DBServer\wallet')  #
+# the connection string copied from the cloud console
+
+print("Successfully connected to Oracle Database")
+
+# Create a table
+with connection.cursor() as cursor:
+    cursor.execute("""
+        begin
+            execute immediate 'drop table todoitem';
+            exception when others then if sqlcode <> -942 then raise; end if;
+        end;""")
+
+    cursor.execute("""
+        create table todoitem (
+            id number generated always as identity,
+            description varchar2(4000),
+            creation_ts timestamp with time zone default current_timestamp,
+            done number(1,0),
+            primary key (id))""")
+
+    print("Table created")
+
+# Insert some data
+with connection.cursor() as cursor:
+    rows = [("Task 1", 0),
+            ("Task 2", 0),
+            ("Task 3", 1),
+            ("Task 4", 0),
+            ("Task 5", 1)]
+
+    cursor.executemany("insert into todoitem (description, done) values(:1, :2)", rows)
+    print(cursor.rowcount, "Rows Inserted")
+
+connection.commit()
+
+# Now query the rows back
+with connection.cursor() as cursor:
+    for row in cursor.execute('select description, done from todoitem'):
+        if (row[1]):
+            print(row[0], "is done")
+        else:
+            print(row[0], "is NOT done")
